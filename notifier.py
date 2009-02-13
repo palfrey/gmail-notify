ab1a19c85d5c7bcb48bb7c7e9905efba90e59366
Tom Parker <palfrey@tevp.net>
2009-02-13 12:59:23 +0000

    Use pynotify rather than custom code

--- notifier.py
+++ notifier.py
@@ -14,8 +14,8 @@ import xmllangs
 import GmailConfig
 import GmailPopupMenu
 import gmailatom
+import pynotify
 
-BKG_PATH=sys.path[0]+"/background.jpg"
 ICON_PATH=sys.path[0]+"/icon.png"
 ICON2_PATH=sys.path[0]+"/icon2.png"
 
@@ -103,42 +103,9 @@ class GmailNotify:
 		self.tray.show_all()
 		# Create the popup menu
 		self.popup_menu = GmailPopupMenu.GmailPopupMenu( self)
-		# Create the popup
-		self.fixed=gtk.Fixed()
-		self.window.add(self.fixed)
-		self.fixed.show()
-		self.fixed.set_size_request(0,0)
-		# Set popup's background image
-		self.image=gtk.Image()
-		self.image.set_from_file( BKG_PATH )
-		self.image.show()
-		self.fixed.put(self.image,0,0)	
-		# Set popup's label
-		self.label=gtk.Label()
-		self.label.set_line_wrap(1)
-		self.label.set_size_request(170,140)
-		self.default_label = "<span size='large' ><i><u>"+self.lang.get_string(21)+"</u></i></span>\n\n\n"+self.lang.get_string(20)
-		self.label.set_markup( self.default_label)
-		# Show popup
-		self.label.show()
-		# Create popup's event box
-		self.event_box = gtk.EventBox()
-		self.event_box.set_visible_window(0)
-		self.event_box.show()
-		self.event_box.add(self.label)
-		self.event_box.set_size_request(180,125)
-		self.event_box.set_events(gtk.gdk.BUTTON_PRESS_MASK)
-		self.event_box.connect("button_press_event", self.event_box_clicked)
-		# Setup popup's event box
-		self.fixed.put(self.event_box,6,25)
-		self.event_box.realize()
-		self.event_box.window.set_cursor(gtk.gdk.Cursor(gtk.gdk.HAND1))
-		# Resize and move popup's event box
-		self.window.resize(180,1)	
-		self.width, self.height = self.window.get_size()
-		self.height+=self.options['voffset']
-		self.width+=self.options['hoffset']
-		self.window.move(gtk.gdk.screen_width() - self.width, gtk.gdk.screen_height() - self.height)
+		pynotify.init("gmail-notify")
+		self.notify = pynotify.Notification(self.lang.get_string(21), "")
+		self.notify.add_action("default", "Default Action", self.event_box_clicked)
 
 		self.init=1
 		while gtk.events_pending():
@@ -171,8 +138,7 @@ class GmailNotify:
 		except:
 			print "login failed, will retry"
 			self._tooltip.set_tip(self.tray,self.lang.get_string(15))
-			self.default_label = "<span size='large' ><u><i>"+self.lang.get_string(15)+"</i></u></span>\n\n"+self.lang.get_string(16)
-			self.label.set_markup(self.default_label)
+			self.notify.update(self.lang.get_string(15),self.lang.get_string(16))
 			self.show_popup()
 			self.dont_connect=0
 			return 0
@@ -208,10 +174,7 @@ class GmailNotify:
 			sender = attrs[2]
 			subject= attrs[3]
 			snippet= attrs[4]
-			if len(snippet)>0:
-				self.default_label="<span size='large' ><u><i>"+self.lang.get_string(17)+sender[0:24]+"</i></u></span>\n"+shortenstring(subject,20)+"\n\n"+snippet+"..."
-			else:
-				self.default_label="<span size='large' ><u><i>"+self.lang.get_string(17)+sender[0:24]+"</i></u></span>\n"+shortenstring(subject,20)+"\n\n"+snippet+"..."
+			self.notify.update(self.lang.get_string(17)+sender[0:24],shortenstring(subject,20)+"\n\n"+snippet+"...")
 			self.show_popup()
 		if attrs[0]>0:
 			print str(attrs[0])+" unread messages"
@@ -221,11 +184,11 @@ class GmailNotify:
 			pixbuf = gtk.gdk.pixbuf_new_from_file( ICON2_PATH )
 		else:
 			print "no new messages"
-			self.default_label="<span size='large' ><i><u>"+self.lang.get_string(21)+"</u></i></span>\n\n\n"+self.lang.get_string(18)
-			self._tooltip.set_tip(self.tray,self.lang.get_string(18))	
+			self._tooltip.set_tip(self.tray,self.lang.get_string(18))
 			pixbuf = gtk.gdk.pixbuf_new_from_file( ICON_PATH )
+			self.notify.update(self.lang.get_string(21),self.lang.get_string(18))
 		
-		self.label.set_markup(self.default_label)
+		#self.notify.show()
 		scaled_buf = pixbuf.scale_simple(24,24,gtk.gdk.INTERP_BILINEAR)
 		self.imageicon.set_from_pixbuf(scaled_buf)
 		self.eventbox.add(self.imageicon)
@@ -269,76 +232,17 @@ class GmailNotify:
 			return (unreadmsgcount,0, sender, subject, finalsnippet)
 
 	def show_popup(self):
-		# If popup is up, destroy it
-		if self.popup==1:
-			self.destroy_popup()
-		# Generate popup
-		print "generating popup"
-		self.popuptimer = gtk.timeout_add(self.options['animationdelay'],self.popup_proc)
-		self.window.show()	
-		return
-
-	def destroy_popup(self):
-		print "destroying popup"
-		if self.popuptimer>0:gtk.timeout_remove(self.popuptimer)
-		if self.waittimer>0: gtk.timeout_remove(self.waittimer)
-		self.senddown=0
-		self.hassettimer=0
-		self.window.hide()
-		self.window.resize(180,1)
-		self.window.move(gtk.gdk.screen_width() - self.width, gtk.gdk.screen_height() - self.height)
-		return
-
-	def popup_proc(self):
-		# Set popup status flag
-		if self.popup==0:
-			self.popup=1
-		currentsize=self.window.get_size()
-		currentposition=self.window.get_position()
-		positiony=currentposition[1]
-		sizey=currentsize[1]
-		if self.senddown==1:
-			if sizey<2:
-				# If popup is down
-				self.senddown=0
-				self.window.hide()
-				self.window.resize(180,1)
-				self.window.move(gtk.gdk.screen_width() - self.width, gtk.gdk.screen_height() - self.height)
-				self.popup=0
-				return gtk.FALSE
-			else:
-				# Move it down
-				self.window.resize(180,sizey-2)	
-				self.window.move(gtk.gdk.screen_width() - self.width,positiony+2)
-		else:
-			if sizey<140:
-				# Move it up
-				self.window.resize(180,sizey+2)
-				self.window.move(gtk.gdk.screen_width() - self.width,positiony-2)
-			else:
-				# If popup is up, run wait timer
-				sizex=currentsize[0]
-				self.popup=1
-				if self.hassettimer==0:
-					self.waittimer = gtk.timeout_add(self.options['popuptimespan'],self.wait)
-					self.hassettimer=1	
-		return gtk.TRUE
-
-	def wait(self):
-		self.senddown=1
-		self.hassettimer=0
-		return gtk.FALSE
+		self.notify.set_timeout(pynotify.EXPIRES_DEFAULT)
+		self.notify.show()
 
 	def tray_icon_clicked(self,signal,event):
 		if event.button==3:
 			self.popup_menu.show_menu(event)
 		else:
-			self.label.set_markup(self.default_label)
 			self.show_popup()
 
-	def event_box_clicked(self,signal,event):
-		if event.button==1:
-			self.gotourl()
+	def event_box_clicked(self, notify, action):
+		self.gotourl()
 
 	def exit(self, event):
 		dialog = gtk.MessageDialog( None, gtk.DIALOG_MODAL, gtk.MESSAGE_QUESTION, gtk.BUTTONS_YES_NO, self.lang.get_string(5))
